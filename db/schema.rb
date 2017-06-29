eb19b57a2b6e4ee83423997b8adfc0c339559a30
Nathan Schulzke <nschulzke@gmail.com>
2017-06-29 17:24:31 +0000

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629161750) do
+ActiveRecord::Schema.define(version: 20170629172311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "base_pokemons", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "health"
+    t.integer  "power"
+    t.integer  "element"
+    t.string   "background"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "moves", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "element"
+    t.integer  "damage"
+    t.integer  "cost"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "players", force: :cascade do |t|
     t.string   "name"
