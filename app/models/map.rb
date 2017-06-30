6c0daf0a292799893d4002aec1e5acf5bad35cae
Nathan Schulzke <nschulzke@gmail.com>
2017-06-30 23:53:54 +0000

    Use better way to create array maps that allows cells to be modified. Add players to main map.

--- app/models/map.rb
+++ app/models/map.rb
@@ -4,12 +4,12 @@ class Map < ApplicationRecord
   
   def self.blank(width: 10, height: 10)
     default_tile = Tile.passable.first
-    Array.new(height, Array.new(width, default_tile))
+    Array.new(height) { Array.new(width) { default_tile } }
   end
   
   def wall(tile = Tile.not_passable.first)
-    map[0] = map.first.map { tile }
-    map[-1] = map.last.map { tile }
+    map.first.map! { tile }
+    map.last.map! { tile }
     
     map.each do |row|
       row[0] = tile
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,18 @@
 class Player < ApplicationRecord
   enum gender: { male: 0, female: 1 }
   has_many :pokemons
+  
+  def self.at(x, y)
+    where(x_pos: x, y_pos: y)
+  end
+  
+  def self.players_map(map)
+    map = Array.new(map.length) { Array.new(map.first.length) }
+    all.each do |player|
+      puts player.name, player.y_pos, player.x_pos
+      map[player.y_pos][player.x_pos] = player
+    end
+    puts map
+    map
+  end
 end
