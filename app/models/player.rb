50ad0e70ced8ca218ea4b2061287b2074d8ce276
Nathan Schulzke <nschulzke@gmail.com>
2017-07-01 00:05:30 +0000

    Remove debugging code

--- app/models/player.rb
+++ app/models/player.rb
@@ -9,10 +9,8 @@ class Player < ApplicationRecord
   def self.players_map(map)
     map = Array.new(map.length) { Array.new(map.first.length) }
     all.each do |player|
-      puts player.name, player.y_pos, player.x_pos
       map[player.y_pos][player.x_pos] = player
     end
-    puts map
     map
   end
 end
