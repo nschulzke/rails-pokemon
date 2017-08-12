78e363cb117586c43cdfa98c2e43e628ad4706ea
Nathan Schulzke <nschulzke@gmail.com>
2017-08-12 16:19:31 -0600

    Make sure that the map is present before streaming

--- app/assets/javascripts/channels/map.js
+++ app/assets/javascripts/channels/map.js
@@ -1,24 +1,27 @@
 $( function() {
-  App.map = App.cable.subscriptions.create({
-    channel: "MapChannel"
-  }, {
-    connected: function() {},
-    disconnected: function() {},
-    received: function(data) {
-      updatePlayers(data['players'])
-    },
-    move: function(direction) {
-      return this.perform('move', { direction: direction })
-    }
-  });
-
   var $map = $('.map');
 
-  function updatePlayers(players) {
-    $map.find('td').html('');
-    for ( var i = 0; i < players.length; ++i ) {
-      var $cell = $map.find('tr').eq(players[i].y_pos).find('td').eq(players[i].x_pos);
-      $cell.html('<span style="color: ' + players[i].background + '">&#9608;</span>');
+  if ( $('.map').length > 0 ) {
+    App.map = App.cable.subscriptions.create({
+      channel: "MapChannel"
+    }, {
+      connected: function() {},
+      disconnected: function() {},
+      received: function(data) {
+        updatePlayers(data['players'])
+      },
+      move: function(direction) {
+        return this.perform('move', { direction: direction })
+      }
+    });
+
+
+    function updatePlayers(players) {
+      $map.find('td').html('');
+      for ( var i = 0; i < players.length; ++i ) {
+        var $cell = $map.find('tr').eq(players[i].y_pos).find('td').eq(players[i].x_pos);
+        $cell.html('<span style="color: ' + players[i].background + '">&#9608;</span>');
+      }
     }
   }
 } );
