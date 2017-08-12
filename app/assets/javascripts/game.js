7058caf96b0092f1cbac7a18949ffb483eeff9bf
Nathan Schulzke <nschulzke@gmail.com>
2017-08-12 14:03:43 -0600

    Implement keyboard movement for player

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,17 +1,24 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require cable
 //= require channels/map
+
+var left  = 37;
+var up    = 38;
+var right = 39;
+var down  = 40;
+$(document).on('turbolinks:load', function() {
+  $(document).on('keydown', function(e) {
+    switch (e.keyCode) {
+      case left:
+        App.map.move('left'); break;
+      case up:
+        App.map.move('up'); break;
+      case right:
+        App.map.move('right'); break;
+      case down:
+        App.map.move('down'); break;
+    }
+  } );
+} );
