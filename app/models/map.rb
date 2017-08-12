f31053c2adb8ad31622028b8b7b744ab8eefc4d3
Nathan Schulzke <nschulzke@gmail.com>
2017-08-11 21:02:24 -0600

    Update maps to not serialize the actual object data--use hash to store tiles (looked up once) and only serialize the object ids

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,20 +1,52 @@
 class Map < ApplicationRecord
   serialize :map
-  require_dependency "app/models/tile"
 
   def self.blank(width: 10, height: 10)
     default_tile = Tile.passable.first
-    Array.new(height) { Array.new(width) { default_tile } }
+    Array.new(height) { Array.new(width) { default_tile.id } }
   end
 
   def wall(tile = Tile.not_passable.first)
-    map.first.map! { tile }
-    map.last.map! { tile }
+    map.first.map! { tile.id }
+    map.last.map! { tile.id }
 
     map.each do |row|
-      row[0] = tile
-      row[-1] = tile
+      row[0] = tile.id
+      row[-1] = tile.id
     end
     save
   end
+
+  def tile tile_id
+    tiles_hash[tile_id]
+  end
+
+  def map_tiles
+    @map_tiles ||= build_map_tiles
+  end
+
+  private
+    def build_tiles_hash
+      tiles_hash = Hash.new
+      map.each do |row|
+        row.each do |tile_id|
+          tiles_hash[tile_id] ||= Tile.find(tile_id)
+        end
+      end
+      tiles_hash
+    end
+
+    def build_map_tiles
+      map_tiles = Array.new(map.length) { Array.new(map.first.length) }
+      map.each_with_index do |row, row_num|
+        row.each_with_index do |tile_id, col_num|
+          map_tiles[row_num][col_num] = tile(tile_id)
+        end
+      end
+      map_tiles
+    end
+
+    def tiles_hash
+      @tiles_hash ||= build_tiles_hash
+    end
 end
