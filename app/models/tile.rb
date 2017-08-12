ba75568dd508355dc0af4be8e2273e491cbdb306
Nathan Schulzke <nschulzke@gmail.com>
2017-08-12 16:49:10 -0600

    Implement default values for tile

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,4 +1,8 @@
 class Tile < ApplicationRecord
+  validates_presence_of :name
+
+  after_initialize :set_defaults, unless: :persisted?
+
   def self.passable
     where(passable: true)
   end
@@ -6,4 +10,10 @@ class Tile < ApplicationRecord
   def self.not_passable
     where(passable: false)
   end
+
+  private
+    def set_defaults
+      self.background ||= 'black'
+      self.passable ||= true
+    end
 end
--- spec/models/tile_spec.rb
+++ spec/models/tile_spec.rb
@@ -4,7 +4,23 @@ RSpec.describe Tile, type: :model do
   it "provides a scope of passable tiles" do
     expect(Tile).to respond_to(:passable)
   end
+
   it "provides a scope of passable tiles" do
     expect(Tile).to respond_to(:not_passable)
+  end
+
+  it "validates presence of name" do
+    tile = Tile.new(background: 'white', passable: false)
+    expect(tile.valid?).to be false
+  end
+
+  it "has a default background" do
+    tile = Tile.new
+    expect(tile.background).to_not be nil
+  end
+
+  it "is passable by default" do
+    tile = Tile.new
+    expect(tile.passable).to be true
   end
 end
